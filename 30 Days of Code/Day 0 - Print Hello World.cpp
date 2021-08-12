65472d2e43aad2f3271ae3935acc05270f905ebb
abhi99555 <80599442+abhi99555@users.noreply.github.com>
2021-08-12 22:35:45 +0530

    updated code

--- 30 Days of Code/Day 0 - Print Hello World.cpp
+++ 30 Days of Code/Day 0 - Print Hello World.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-  std::cout << "Hello World." << std::endl << "Welcome to 30 Days of Code." << std::endl;
-  return 0;
-}
+ cout<<"Hello World."<<endl<<"welcome to 30 days of code."
